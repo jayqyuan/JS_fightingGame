346d5e7443a94995e1ff60d434d6989e7610fee3
jayqyuan <118870651+jayqyuan@users.noreply.github.com>
2023-03-11 14:34:04 -0500

    set game timeout and gamer over factors

--- index.js
+++ index.js
@@ -126,6 +126,35 @@ function rectangularCollision({rectangle1, rectangle2}){
             rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height)
 }
 
+function determineWinner({player, enemy, timerId}){
+    clearTimeout(timerId)
+    document.querySelector("#displayText").style.display = "flex";
+     if(player.health === enemy.health){
+            document.querySelector('#displayText').innerHTML = 'Tie';
+        } else if (player.health > enemy.health){
+            document.querySelector('#displayText').innerHTML = 'player 1 wins';
+        } else if(player.health < enemy.health) {
+            document.querySelector("#displayText").innerHTML = "player 2 wins";
+        }
+}
+
+let timer = 60
+let timerId
+function decreaseTimer(){
+    
+    if(timer > 0){
+        timerId = setTimeout(decreaseTimer, 1000)
+        timer --
+        document.querySelector('#timer').innerHTML = timer
+    }
+    if(timer === 0){
+        
+        determineWinner({player, enemy, timerId})
+        }
+    }
+
+decreaseTimer()
+
 function animate() {
     window.requestAnimationFrame(animate)
     
@@ -176,7 +205,12 @@ function animate() {
             enemy.isAttacking = false
             player.health -= 20
             document.querySelector('#playerHealth').style.width= player.health + '%'
-        }    
+        }
+    
+    //end game based on health
+    if(enemy.health <= 0 || player.health <= 0 ){
+        determineWinner({player, enemy, timerId})
+    }
 }
 
 
